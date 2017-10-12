3caa7a6a8c8116ade2994c34541a0ebfec0cccad
eshulga <ewgenij.shulga@gmail.com>
2017-10-12 23:02:26 +0300

    add module 5

---
+++ module5/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        autoplay: false,
+        nextArrow: '#slider-nav-next',
+        prevArrow: '#slider-nav-prev',
+    });
+
+    var scroll_top_duration = 800;
+
+    $('#to-top').on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
+
+})
